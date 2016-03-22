b689cf837255b969c86203728780443a0f2eee47
aphinith <aphinith84@gmail.com>
2016-03-22 12:32:42 -0700

    added dancers

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,16 +1,18 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps){
   //inherit properties from Dancer Super Class
-  Dancer.call(this, top, left, timeBetweenSteps);
+  makeDancer.call(this, top, left, timeBetweenSteps);
   //invoke makeBlinkyDancer.step below
+
   this.step();
 };
 
-  makeBlinkyDancer.prototype = Object.create(Dancer.prototype);
+  makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
 
   makeBlinkyDancer.prototype.step = function(){
     
     //invokes the step method on makeDancer class with 'this' class as the context
-    Dancer.prototype.step.call(this);
+    makeDancer.prototype.step.call(this);
+    makeDancer.prototype.setPosition.call(this, this.top, this.left);
 
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and
@@ -20,3 +22,19 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps){
     //toggles the position of the node 'dancer' whenever the interval time has ended
     this.$node.toggle();
   };
+
+ var Deadpool = function(top, left, timeBetweenSteps){
+  //inherit properties from Dancer Super Class
+  makeBlinkyDancer.call(this, top, left, timeBetweenSteps);
+  //invoke makeBlinkyDancer.step below
+  this.$node = $('<span class="deadpool"></span>');
+  this.step();
+}; 
+
+  Deadpool.prototype = Object.create(makeBlinkyDancer.prototype);
+
+
+var Bananas = new makeBlinkyDancer();
+var SpongeBob = new makeBlinkyDancer();
+
+
--- src/dancer.js
+++ src/dancer.js
@@ -1,16 +1,19 @@
 // Creates and returns a new dancer object that can step
-var Dancer = function(top, left, timeBetweenSteps){
+var makeDancer = function(top, left, timeBetweenSteps){
   this.timeBetweenSteps = timeBetweenSteps;
+  this.top = top;
+  this.left = left;
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
 };
 
-  Dancer.prototype.step = function(){
+  //makeDancer.psetPosition(this.top, this.left);
+
+
+  makeDancer.prototype.step = function(){
     // the basic dancer doesn't do anything interesting at all on each step,
     // it just schedules the next step
     var mBD = this
-
-    console.log('mD:', this);
 
     //Set the timeout  of the subclass binding 'this' to the subclass with each call  
     setTimeout(function(){mBD.step()}, this.timeBetweenSteps)
@@ -18,7 +21,7 @@ var Dancer = function(top, left, timeBetweenSteps){
 
   // makeDancer.prototype.step();
 
-  Dancer.prototype.setPosition = function(top, left){
+  makeDancer.prototype.setPosition = function(top, left){
     // Use css top and left properties to position our <span> tag
     // where it belongs on the page. See http://api.jquery.com/css/
     
@@ -34,7 +37,6 @@ var Dancer = function(top, left, timeBetweenSteps){
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  // makeDancer.setPosition(this.top, this.left);
 
 
   //instantiating makeDancer
